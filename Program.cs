b421b993a3f115198f967db98c3bc936a8c68f85
Nachosisk <ilya.smolkov155@gmail.com>
2025-09-14 20:02:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("введите месяц");
+string a = Console.ReadLine();
+switch (a)
+{
+    case "Январь":
+        Console.WriteLine("зима");
+        break;
+    case "Февраль":
+        Console.WriteLine("зима");
+        break;
+       case "Декабрь":
+        Console.WriteLine("зима");
+        break;
+    case "Март":
+        Console.WriteLine("весна");
+        break;
+    case "Апрель":
+        Console.WriteLine("весна");
+        break;
+    case "Май":
+        Console.WriteLine("весна");
+        break;
+    case "Июнь":
+        Console.WriteLine("лето");
+        break;
+    case "Июль":
+        Console.WriteLine("лето");
+        break;
+    case "Август":
+        Console.WriteLine("лето");
+        break;
+    case "Сентябрь":
+        Console.WriteLine("осень");
+        break;
+    case "Октябрь":
+        Console.WriteLine("осень");
+        break;
+    case "Ноябрь":
+        Console.WriteLine("осень");
+        break;
+}
+    
